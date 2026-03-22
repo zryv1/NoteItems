c78025117991297db9c08889be8b3a224322f724
zryv1 <198713594+zryv1@users.noreply.github.com>
2026-03-23 00:41:15 +0300

    add project_model and projects_storage files

---
+++ note_items_app/lib/project_model.dart
@@ -0,0 +1,6 @@
+class ProjectModel {
+  final int id;
+  final String name;
+
+  ProjectModel({required this.id, required this.name});
+}
---
+++ note_items_app/lib/projects_storage.dart
@@ -0,0 +1,11 @@
+import "dart:convert";
+import "project_model.dart";
+import "package:shared_preferences/shared_preferences.dart";
+
+class ProjectsStorage {
+  final sharedPrefs = SharedPreferences.getInstance();
+
+  void createProject() {
+
+  }
+}
